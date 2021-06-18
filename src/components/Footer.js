1cac9d30069eb697127571ff5ee1bc571b9ae99c
TSENG KWEIMING <tsengkweiming@team-lab.com>
2021-06-18 11:24:06 +0900

    take contact off

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -40,21 +40,6 @@ const Footer = class extends React.Component {
                         Products
                       </Link>
                     </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
                   </ul>
                 </section>
               </div>
